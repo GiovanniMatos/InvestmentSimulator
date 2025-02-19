70274d885063ef6449d0a019ef43dfb63bb9e202
GiovanniMatos <giovannyafonso23@gmail.com>
2025-02-19 16:24:33 -0400

    calc functions!

--- front-end/src/app/components/ButtonSubmit/ButtonSubmit.jsx
+++ front-end/src/app/components/ButtonSubmit/ButtonSubmit.jsx
@@ -1,55 +1,42 @@
-
-
 export default function ButtonSubmit({ 
-        aporteInicial, 
-        aporteMensal, 
-        prazo, 
-        selecionado, 
-        taxaCDI }){
+    aporteInicial, 
+    aporteMensal, 
+    prazo, 
+    selecionado, 
+    taxaCDI 
+}) {
     
     const valorInicial = Number(aporteInicial);
-    const valorMensal = Number(aporteMensal) ;
+    const valorMensal = Number(aporteMensal);
     const valorPrazo = Number(prazo);
     const valorTempoSelecionado = selecionado;
     const valorTaxaCDI = Number(taxaCDI);
 
-    const valorCDI = (valorTaxaCDI / 100) * 10.39;
-    const taxaMensalCDB = (valorCDI / 12) / 100 ;
-    const meses = valorPrazo * 12;
-    const totalMensal = valorMensal * meses
+    const valorCDI = (valorTaxaCDI / 100) * 13.15;
+    const taxaMensalCDB = (valorCDI / 12) / 100; 
+    const meses = valorTempoSelecionado === "ano(s)" ? valorPrazo * 12 : valorPrazo; 
 
-    function calculaJurosCompostosAnos(){
-        const montante = valorInicial + valorMensal * (1 + taxaMensalCDB) ** meses;
-        return montante;
-    }
-    // function calculaJurosCompostosMeses(){
-    //     return montante;
-    // }
-
-    let resultado = 0;
-    
-    switch (valorTempoSelecionado) {
-        case "ano(s)":
-            resultado = calculaJurosCompostosAnos();
-            break;
-        case "mes(es)":
-            resultado = calculaJurosCompostosMeses();
-            break;
-        default:
-            break;
+    function calculaJurosCompostos() {
+        const montanteInicial = valorInicial * Math.pow((1 + taxaMensalCDB), meses);
+        const montanteMensal = valorMensal * ((Math.pow((1 + taxaMensalCDB), meses) - 1) / taxaMensalCDB);
+        
+        return montanteInicial + montanteMensal;
     }
 
-    return(
+    const resultado = calculaJurosCompostos();
+
+    return (
         <div>
             <button 
-            type="button" 
-            className="flex bg-orange-500
-             hover:bg-orange-600 pl-4
-             pt-2 pb-2 pr-4 rounded text-white 
-             font-bold mt-2 mx-auto text-center">Calcular</button>
+                type="button" 
+                className="flex 
+                bg-orange-500 hover:bg-orange-600
+                 pl-4 pt-2 pb-2 pr-4 rounded text-white
+                  font-bold mt-2 mx-auto text-center">
+                Calcular
+            </button>
 
-             <p>{resultado}</p>
-             <p>{totalMensal}</p>
+            <p>Valor Futuro do Investimento: R$ {resultado.toFixed(2)}</p>
         </div>
-    )
+    );
 }
